f69d5884788a85f770effc23065591d6e8c7cfc9
Matthew Grill <matt@fourkitchens.com>
2014-08-08 10:58:49 -0700

    Updated shell provisioner.

--- vagrant/drupal-setup/install.sh
+++ vagrant/drupal-setup/install.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 set -e
 
+cd /home/vagrant/finished/drupal
+
 drush -y make --no-cache setup/site.make www
 cd www
-drush -y si --account-name=admin --account-pass=admin --db-url=mysql://fk:fkbuild@localhost/trainingdrupal
+drush -y si --account-name=admin --account-pass=admin --db-url=mysql://root:pass@localhost/trainingdrupal
 sed -i "" 's/\# RewriteBase \//RewriteBase \//' .htaccess
 cp -R ../setup/modules/blog_migrate sites/all/modules/custom/
 mkdir -p sites/default/files/migration/blog
