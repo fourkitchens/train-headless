b5f11043bf59740c81a44ae6e79fcf1b81f9b23f
Matthew Grill <matt@fourkitchens.com>
2014-08-12 11:14:29 -0700

    make things a little more sensible. delete www if it exists.

--- vagrant/drupal-setup/install.sh
+++ vagrant/drupal-setup/install.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 set -e
 
+WWW_PATH="/home/vagrant/finished/drupal/www"
+
+if [ -d "$WWW_PATH" ]; then
+  sudo rm -rf $WWW_PATH
+fi
+
 /home/vagrant/.composer/vendor/bin/drush -y make --no-cache /home/vagrant/finished/drupal/drupal-setup/setup/site.make www
 
-cd /home/vagrant/finished/drupal/www/
+cd $WWW_PATH
 /home/vagrant/.composer/vendor/bin/drush -y si --account-name=admin --account-pass=admin --db-url=mysql://sqluser:sqluser@localhost/drupal_finished
 
-sed -i 's/\# RewriteBase \//RewriteBase \//' /home/vagrant/finished/drupal/www/.htaccess
+sed -i 's/\# RewriteBase \//RewriteBase \//' $WWW_PATH/.htaccess
 
-cp -R /home/vagrant/finished/drupal/drupal-setup/setup/modules/fkblog /home/vagrant/finished/drupal/www/sites/all/modules/custom/
-mkdir -p /home/vagrant/finished/drupal/www/sites/default/files/migration/blog
-cp /home/vagrant/finished/drupal/drupal-setup/setup/migration/blog/blog.xml /home/vagrant/finished/drupal/www/sites/default/files/migration/blog
+cp -R /home/vagrant/finished/drupal/drupal-setup/setup/modules/fkblog $WWW_PATH/sites/all/modules/custom/
+mkdir -p $WWW_PATH/sites/default/files/migration/blog
+cp /home/vagrant/finished/drupal/drupal-setup/setup/migration/blog/blog.xml $WWW_PATH/sites/default/files/migration/blog
 
 /home/vagrant/.composer/vendor/bin/drush -y en fkblog
 /home/vagrant/.composer/vendor/bin/drush mar
