71502e7e7ace02472bee9d7facfc1f6e522f9c19
Matthew Grill <matt@fourkitchens.com>
2014-09-07 12:24:49 -0700

    Render JSON with DustJS

---
+++ finished/node/006-dust/app.js
@@ -0,0 +1,21 @@
+var express         = require('express'),
+    app             = express(),
+    request         = require('request'),
+    drupalUrl       = '{drupal.services.url}',
+    dust            = require('./dist/index.js');
+
+app.get('/:nid', function(req, res){
+  var options = {
+    'url'   : drupalUrl + req.params.nid,
+    'json'  : true
+  };
+  request(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      dust.render('index.dust', body, function(err, html_out) {
+        res.send(html_out);
+      });
+    }
+  })
+});
+
+app.listen(3000);
