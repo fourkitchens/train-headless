cdc4f21028cbdf9d6cf1083699de6010a0babe88
Michal Minecki <michal@mirzu.com>
2014-09-08 11:22:45 -0500

    updated the install script

--- vagrant/drupal-setup/install.sh
+++ vagrant/drupal-setup/install.sh
@@ -15,10 +15,14 @@ cd $WWW_PATH
 sed -i 's/\# RewriteBase \//RewriteBase \//' $WWW_PATH/.htaccess
 
 mkdir -p $WWW_PATH/sites/all/modules/custom
-cp -R /home/vagrant/finished/drupal/drupal-setup/setup/modules/fkblog $WWW_PATH/sites/all/modules/custom
+mkdir -p $WWW_PATH/sites/all/modules/features
+cp -R /home/vagrant/finished/drupal/drupal-setup/setup/modules/fkblog $WWW_PATH/sites/all/modules/custom/fkblog
+cp -R /home/vagrant/finished/drupal/drupal-setup/setup/modules/fkblog_features $WWW_PATH/sites/all/modules/features/fkblog
+cp -R /home/vagrant/finished/drupal/drupal-setup/setup/modules/services_hal $WWW_PATH/sites/all/modules/custom/services_hal
 mkdir -p $WWW_PATH/sites/default/files/migration/blog
 cp /home/vagrant/finished/drupal/drupal-setup/setup/migration/blog/blog.xml $WWW_PATH/sites/default/files/migration/blog
 
 /home/vagrant/.composer/vendor/bin/drush -y en fkblog
 /home/vagrant/.composer/vendor/bin/drush mar
 /home/vagrant/.composer/vendor/bin/drush mi BlogPost
+/home/vagrant/.composer/vendor/bin/drush -y composer-manager install
