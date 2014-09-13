5f3d381adda73506ee288e61ed54cf85ae8416fb
Matthew Grill <matt@fourkitchens.com>
2014-09-13 16:15:18 -0700

    Caching Drupal Content in Redis.

---
+++ finished/node/008-redis/app.js
@@ -0,0 +1,54 @@
+var express         = require('express'),
+    app             = express(),
+    request         = require('request'),
+    drupalUrl       = 'http://test.dev/drupal.json',
+    redis           = require('redis'),
+    client          = redis.createClient();
+    dust            = require('./dist/index.js');
+
+var helpers         = {
+      key_exists    : function (key,callback) {
+        client.exists(key, function (err, doesExist) {
+          switch(doesExist){
+            case 0: // the key does not exist
+              callback(false, {});
+              break;
+            case 1: // the key exists
+              client.hgetall(key, function (err, obj){
+                callback(true, obj);
+              });
+              break
+          }
+        });
+      },
+      key_set       : function (key, data, callback) {
+        client.hmset(key, data, callback);
+      }
+    };
+
+app.get('/:nid', function(req, res){
+  var options = {
+    //'url'   : drupalUrl + req.params.nid,
+    'url'   : drupalUrl,
+    'json'  : true
+  };
+  helpers.key_exists('node:'+req.params.nid, function(status, data) {
+    if (!status) {
+      request(options, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+          dust.render('index.dust', body, function(err, html_out) {
+            helpers.key_set('node:'+req.params.nid,{'body' : JSON.stringify(body), 'html' : html_out}, function(){
+              res.send(html_out);
+              client.expire('node:'+req.params.nid, 60);
+            })
+          });
+        }
+      });
+    }
+    else {
+      res.send(data.html);
+    }
+  });
+});
+
+app.listen(3000);
