38452f9b0deb68700565923ec1589c8be24a4923
Matthew Grill <matt@fourkitchens.com>
2015-09-10 13:41:39 -0700

    DCB Updates
    Updates on names to reflect DupalCon Barcelona

--- finished/node/introduction-to-express/app.js
+++ finished/node/introduction-to-express/app.js
@@ -35,7 +35,7 @@
   });
 
   app.get('/example', function (req, res) {
-    res.send('Welcome to NYC Camp.');
+    res.send('Welcome to Drupal Con Barcelona.');
   });
 
   app.get('/api', function (req, res) {
--- finished/node/introduction-to-express/posts.js
+++ finished/node/introduction-to-express/posts.js
@@ -3,15 +3,15 @@ module.exports = {
   posts: [
     {
       'id': 1,
-      'title': 'Welcome to NYC Camp',
-      'body': 'This is the first entry in my NYC Camp travel log.',
-      'published': '7/16/2015'
+      'title': 'Welcome to Drupal Con Barcelona',
+      'body': 'This is the first entry in my Drupal Con Barcelona travel log.',
+      'published': '9/21/2015'
     },
     {
       'id': 2,
-      'title': 'NYC Camp is over.',
-      'body': 'On my way home from NYC Camp',
-      'published': '7/19/2015'
+      'title': 'Drupal Con Barcelona is over.',
+      'body': 'On my way home from Drupal Con Barcelona',
+      'published': '9/25/2015'
     }
   ]
 }
